20a314b3f2f022fcd0447838d81c82deaf258b4d
shunlog <artiombalan331@gmail.com>
2025-08-08 13:57:26 +0300

    add leaves

--- hex-tree.rkt
+++ hex-tree.rkt
@@ -10,9 +10,12 @@
 
 
 ;;; draws a branch straight, then recurses
-;;; - scales down at each step with (scale)
+;;; At each recursive step it:
+;;; - scales down the length using (scale-len)
+;;; - scales down the width using (scale-w)
+;;; - terminates if (terminate?) returns #t
 (define
-  (hex-tree n t [depth 0] [len-scale 1] [w-scale 1]
+  (hex-tree lim t [depth 0] [len-scale 1] [w-scale 1]
             #:angle [ang 60]
             #:start-len [len0 80]
             #:start-w [w0 7]
@@ -20,22 +23,34 @@
             [scale-len
              (λ (old) (* old (random-ref '(0.5 1))))]
             #:scale-w [scale-w (λ (old) (* old 0.75))]
-            #:terminate? [terminate? (λ () (< 0.9 (random)))])
+            #:terminate? [terminate? (λ () (< 0.9 (random)))]
+            #:leaves? [leaves? #t])
   (define len (* len0 len-scale))
   (define new-len-scale (scale-len len-scale))
   (define w (* w0 w-scale))
   (define new-w-scale (scale-w w-scale))
   (define (recurse t)
     (hex-tree
-     n t (add1 depth) new-len-scale new-w-scale
+     lim t (add1 depth) new-len-scale new-w-scale
      #:angle ang #:start-len len0 #:start-w w0
      #:scale-len scale-len #:scale-w scale-w
-     #:terminate? terminate?))
-  (if (or (= n depth) (terminate?))
-      t
+     #:terminate? terminate?
+     #:leaves? leaves?))
+  (define (draw-branch t)
+    (~> t
+        (set-pen-width w)
+        (draw len _)))
+  (define (draw-leaf t)
+    (define radius (+ 2 (* w-scale 16))) ; through trial and error
+    (regular-poly (quotient 360 ang) radius t))
+  (if (or (= lim depth) (terminate?))
+      (if leaves?
+          (~> t
+              (draw-branch _)
+              (draw-leaf _))
+          t)
       (~> t
-          (set-pen-width w)
-          (draw len _)
+          (draw-branch _)
           (send-off
            (λ~> (turn ang _)
                 (recurse)))
@@ -59,7 +74,8 @@
             #:start-w 10
             #:scale-len (λ (old) (* old (random-ref '(0.6 1))))
             #:scale-w (λ (old) (* old 0.6))
-            #:terminate? (λ () (< 0.9 (random)))))])
+            #:terminate? (λ () (< 0.9 (random)))
+            #:leaves? #t))])
     (~> t0
         (send-off
          (λ~> hex-tree-instance))
